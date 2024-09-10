43a887bb112169fbe5d213d4faed7c59eb105805
Eleonora Trikidou <trikidoue@gmail.com>
2024-09-10 11:33:53 +0300

    Update Address.java

--- src/main/java/com/example/Trikidou_SWA/model/Address.java
+++ src/main/java/com/example/Trikidou_SWA/model/Address.java
@@ -1,35 +1,38 @@
 package com.example.Trikidou_SWA.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Address {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String workAddress;
     private String homeAddress;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 	public String getWorkAddress() {
 		return workAddress;
 	}
+
 	public void setWorkAddress(String workAddress) {
 		this.workAddress = workAddress;
 	}
+
 	public String getHomeAddress() {
 		return homeAddress;
 	}
+
 	public void setHomeAddress(String homeAddress) {
 		this.homeAddress = homeAddress;
 	}
-
+    
     
 }
