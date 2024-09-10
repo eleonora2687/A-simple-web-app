07b3c9dcda9149ba42951338ce702e90002faf3c
Eleonora Trikidou <trikidoue@gmail.com>
2024-09-10 11:45:18 +0300

    Update display_users.js

--- src/main/webapp/js/display_users.js
+++ src/main/webapp/js/display_users.js
@@ -1,29 +1,52 @@
 $(document).ready(function() {
-    $.getJSON("/api/users", function(users) {
-        var rows = '';
-        $.each(users, function(index, user) {
-            rows += '<tr>';
-            rows += '<td>' + user.name + '</td>';
-            rows += '<td>' + user.surname + '</td>';
-            rows += '<td><button onclick="viewUser(' + user.id + ')">View</button>';
-            rows += '<button onclick="deleteUser(' + user.id + ')">Delete</button></td>';
-            rows += '</tr>';
-        });
-        $('#usersTableBody').html(rows);
-    });
-});
-
-function viewUser(id) {
-    window.open('/user_details.html?id=' + id, '_blank');
-}
-
-function deleteUser(id) {
     $.ajax({
-        type: "DELETE",
-        url: "/api/users/" + id,
-        success: function() {
-            alert("User deleted successfully!");
-            location.reload();
+        url: '/api/users',
+        method: 'GET',
+        success: function(users) {
+            users.forEach(function(user) {
+                $("#usersTableBody").append(`
+                    <tr>
+                        <td>${user.name}</td>
+                        <td>${user.surname}</td>
+                        <td>
+                            <button class="button view-button" data-id="${user.id}">View</button>
+                            <button class="button edit-button" data-id="${user.id}">Edit</button>
+                            <button class="button delete-button" data-id="${user.id}">Delete</button>
+                        </td>
+                    </tr>
+                `);
+            });
+
+            $(".view-button").click(function() {
+                var userId = $(this).data('id');
+                window.open(`user_details.html?id=${userId}`, '_blank');
+            });
+
+            $(".edit-button").click(function() {
+                var userId = $(this).data('id');
+                window.open(`edit_user.html?id=${userId}`, '_blank');
+            });
+
+            // Handle delete button click
+            $(".delete-button").click(function() {
+                var userId = $(this).data('id');
+                if (confirm("Are you sure you want to delete this user?")) {
+                    $.ajax({
+                        url: `/api/users/${userId}`,
+                        method: 'DELETE',
+                        success: function() {
+                            alert("User and associated address deleted successfully!");
+                            location.reload(); // Reload the page after deletion
+                        },
+                        error: function(xhr, status, error) {
+                            alert("Error occurred while deleting the user.");
+                        }
+                    });
+                }
+            });
+        },
+        error: function() {
+            alert("Failed to fetch users.");
         }
     });
-}
+});
