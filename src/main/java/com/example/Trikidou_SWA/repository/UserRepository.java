b77f15a007f8e2423cd591302e642ea5c196be46
Eleonora Trikidou <trikidoue@gmail.com>
2024-09-10 11:35:59 +0300

    Update UserRepository.java

--- src/main/java/com/example/Trikidou_SWA/repository/UserRepository.java
+++ src/main/java/com/example/Trikidou_SWA/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.example.Trikidou_SWA.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.Trikidou_SWA.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    @Query("SELECT MAX(u.id) FROM User u")
+    Optional<Long> findMaxId();
+}
