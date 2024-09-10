1190878baa2204b49a102ae4ffa119d16fb3525d
Eleonora Trikidou <trikidoue@gmail.com>
2024-09-10 11:35:21 +0300

    Update AddressRepository.java

--- src/main/java/com/example/Trikidou_SWA/repository/AddressRepository.java
+++ src/main/java/com/example/Trikidou_SWA/repository/AddressRepository.java
@@ -1,7 +1,12 @@
 package com.example.Trikidou_SWA.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.Trikidou_SWA.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 
-public interface AddressRepository extends JpaRepository<Address, Long> {}
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    
+    @Query("SELECT MAX(a.id) FROM Address a")
+    Optional<Long> findMaxId();
+}
